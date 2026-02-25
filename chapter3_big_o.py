fd162f50d47a5694c81e859257e8b4ee316c10cb
Gabriel Valenga <gabrielvalengalemos@yahoo.com>
2026-02-25 19:56:44 -0300

    feat: Created new examples using list functions; Refactored the functions_comparison function to be reusable

--- chapter3_big_o.py
+++ chapter3_big_o.py
@@ -1,5 +1,27 @@
 import timeit
 
+class General:
+
+    @staticmethod
+    def functions_comparison(func1, func2, *args, number=1_000_000):
+        print(f"{func1.__name__} result: {func1(*args)}")
+        print(f"{func2.__name__} result: {func2(*args)}")
+
+        time1 = timeit.timeit(lambda: func1(*args), number=number)
+        time2 = timeit.timeit(lambda: func2(*args), number=number)
+
+        print(f"Time taken by {func1.__name__}: {time1:.6f} seconds")
+        print(f"Time taken by {func2.__name__}: {time2:.6f} seconds")
+
+        if time2 < time1:
+            speedup = time1 / time2
+            print(f"{func2.__name__} is {speedup:.2f}x faster than {func1.__name__}")
+        elif time1 < time2:
+            speedup = time2 / time1
+            print(f"{func1.__name__} is {speedup:.2f}x faster than {func2.__name__}")
+        else:
+            print("Both functions have the same execution time")
+
 class SumFunctions:
 
     # n+1 steps to big O notation
@@ -21,21 +43,22 @@ class SumFunctions:
         return (n * (n + 1)) / 2
 
 
+class ListFunctions:
 
-    def sum_functions_comparison(self, n):
-        print(f"Sum1 result: {self.sum1(n)}")
-        print(f"Sum2 result: {self.sum2(n)}")
-        time_sum_1 = timeit.timeit(lambda: self.sum1(n), globals=globals(), number=1000000)
-        time_sum_2 = timeit.timeit(lambda: self.sum2(n), globals=globals(), number=1000000)
-        print(f"Time taken by sum1: {time_sum_1} seconds")
-        print(f"Time taken by sum2: {time_sum_2} seconds")
-        if time_sum_2 < time_sum_1:
-            speedup = time_sum_1 / time_sum_2
-            print(f"sum_2 is {speedup:.2f}x faster than sum_1")
-        elif time_sum_1 < time_sum_2:
-            speedup = time_sum_2 / time_sum_1
-            print(f"sum_1 is {speedup:.2f}x faster than sum_2")
-        else:
-            print("Both functions have the same execution time")
+    # O(n) because the number of steps grows linearly with n
+    # Example: if n = 10, the function will execute 10 steps (0 to 9)
+    @staticmethod
+    def list1(n):
+        list = []
+        for i in range(n):
+            list += [i]
+        return list
+    
+    # O(1) because the number of steps is constant and does not grow with n
+    # Example: if n = 10, the function will execute 1 step (creating a range object with 10 elements)
+    @staticmethod
+    def list2(n):
+        return range(n)
 
-SumFunctions().sum_functions_comparison(10)
+General().functions_comparison(SumFunctions.sum1, SumFunctions.sum2, 10)
+General().functions_comparison(ListFunctions.list1, ListFunctions.list2, 10)
