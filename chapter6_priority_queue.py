42d61c77499451cef48a57bc3b60a5c1b55d60b6
Gabriel Valenga <gabrielvalengalemos@yahoo.com>
2026-03-16 09:00:16 -0300

    feat: Created file with implementation of priority queues

---
+++ chapter6_priority_queue.py
@@ -0,0 +1,93 @@
+import numpy as np
+
+class PriorityQueue:
+
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.number_of_elements = 0
+        self.values = np.empty(self.capacity, dtype=int)
+
+
+    def __queue_is_empty(self):
+        return self.number_of_elements == 0
+    
+
+    def __queue_is_full(self):
+        return self.number_of_elements == self.capacity
+    
+
+    def enqueue(self, value):
+        if self.__queue_is_full():
+            print('The queue is full!')
+            return 
+
+        if self.number_of_elements == 0:
+            self.values[self.number_of_elements] = value
+            self.number_of_elements += 1
+        else:
+            x = self.number_of_elements - 1
+            while x >= 0:
+                if value > self.values[x]:
+                    self.values[x + 1] = self.values[x]
+                else:
+                    break 
+                x -=1
+            self.values[x+1] = value
+            self.number_of_elements += 1
+
+
+    def dequeue(self):
+        if self.__queue_is_empty():
+            print('The queue is empty!')
+            return 
+        value = self.values[self.number_of_elements-1]
+        self.number_of_elements -= 1
+        return value
+
+
+
+    def first(self):
+        if self.__queue_is_empty():
+            return -1 
+        return self.values[self.number_of_elements - 1]
+    
+
+    def return_queue_in_format_of_a_list(self):
+        return self.values.tolist()
+    
+
+queue = PriorityQueue(5)
+print(queue.first())
+queue.enqueue(30)
+print(queue.return_queue_in_format_of_a_list())
+print(queue.first())
+queue.enqueue(50)
+print(queue.return_queue_in_format_of_a_list())
+print(queue.first())
+queue.enqueue(10)
+print(queue.return_queue_in_format_of_a_list())
+print(queue.first())
+queue.enqueue(40)
+print(queue.return_queue_in_format_of_a_list())
+print(queue.first())
+queue.enqueue(20)
+print(queue.return_queue_in_format_of_a_list())
+print(queue.first())
+queue.enqueue(2)
+queue.dequeue()
+print(queue.return_queue_in_format_of_a_list())
+print(queue.first())
+queue.dequeue()
+print(queue.return_queue_in_format_of_a_list())
+print(queue.first())
+queue.dequeue()
+print(queue.return_queue_in_format_of_a_list())
+print(queue.first())
+queue.enqueue(5)
+print(queue.return_queue_in_format_of_a_list())
+print(queue.first())
+
+
+
+
+
