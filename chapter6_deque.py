0d6781637a693ab8976ee04062fd02d5541e4793
Gabriel Valenga <gabrielvalengalemos@yahoo.com>
2026-03-20 08:44:16 -0300

    feat: Creating functions to retrieve values from start and end position of deque

--- chapter6_deque.py
+++ chapter6_deque.py
@@ -4,77 +4,91 @@ class Deque:
 
     def __init__(self, capacity):
         self.capacity = capacity
-        self.start = 0
-        self.end = -1
+        self.start_position = 0
+        self.end_position = -1
         self.number_of_elements = 0
         self.values = np.empty(self.capacity, dtype=int)
 
 
     def __deque_is_full(self):
-        return (self.start == 0 and self.end == self.capacity - 1) or (self.start == self.end + 1)
+        return (self.start_position == 0 and self.end_position == self.capacity - 1) or (self.start_position == self.end_position + 1)
     
 
     def __deque_is_empty(self):
-        return self.start == -1
+        return self.start_position == -1
     
 
-    def insert_value_at_start(self, value:int):
+    def insert_value_at_start_position(self, value:int):
         if self.__deque_is_full():
             print('Deque is full!')
             return
         if self.__deque_is_empty():
-            self.start = 0
-            self.end = 0
-        #if starts at first position
-        elif self.start == 0:
-            self.start = self.capacity - 1
+            self.start_position = 0
+            self.end_position = 0
+        #if start_positions at first position
+        elif self.start_position == 0:
+            self.start_position = self.capacity - 1
         else:
-            self.start -= 1
+            self.start_position -= 1
 
-        self.values[self.start] = value
+        self.values[self.start_position] = value
     
 
-    def insert_value_at_end(self, value:int):
+    def insert_value_at_end_position(self, value:int):
         if self.__deque_is_full():
             print('Deque is full!')
             return
         if self.__deque_is_empty():
-            self.start = 0
-            self.end = 0
-        # if ends at last position
-        elif self.end == self.capacity - 1:
-            self.end = 0
+            self.start_position = 0
+            self.end_position = 0
+        # if end_positions at last position
+        elif self.end_position == self.capacity - 1:
+            self.end_position = 0
         else:
-            self.end += 1
-        self.values[self.end] = value
+            self.end_position += 1
+        self.values[self.end_position] = value
 
 
-    def delete_from_start(self):
+    def delete_from_start_position(self):
         if self.__deque_is_empty():
             print('Deque is empty!')
             return 
         #if has only one element
-        if self.start == self.end:
-            self.start = -1
-            self.end = -1
-        elif self.start == self.capacity - 1: #back to start position
-            self.start = 0
+        if self.start_position == self.end_position:
+            self.start_position = -1
+            self.end_position = -1
+        elif self.start_position == self.capacity - 1: #back to start_position position
+            self.start_position = 0
         else:
-            #increase start position to remove current start position
-            self.start += 1
+            #increase start_position position to remove current start_position position
+            self.start_position += 1
             
 
-
-    def delete_from_end(self):
+    def delete_from_end_position(self):
         if self.__deque_is_empty():
             print('Deque is empty!')
             return 
         #if has only one element
-        if self.start == self.end:
-            self.start = -1
-            self.end = -1
-        elif self.start == 0: #back to end position
-            self.end = self.capacity - 1
+        if self.start_position == self.end_position:
+            self.start_position = -1
+            self.end_position = -1
+        elif self.start_position == 0: #back to end_position position
+            self.end_position = self.capacity - 1
         else:
-            #increase end position to remove current end position
-            self.end -= 1    
+            #increase end_position position to remove current end_position position
+            self.end_position -= 1    
+
+
+    def get_start_value(self):
+        if self.__deque_is_empty():
+            print('Deque is empty!')
+            return 
+        return self.values[self.start_position]
+    
+
+    def get_end_value(self):
+        if self.__deque_is_empty():
+            print('Deque is empty!')
+            return 
+        return self.values[self.end_position]
+    
