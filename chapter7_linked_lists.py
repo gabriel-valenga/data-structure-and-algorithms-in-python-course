53f0be219ca743af252a3c47f453858f39bab0e9
Gabriel Valenga <gabrielvalengalemos@yahoo.com>
2026-03-21 17:28:44 -0300

    feat: Created file for linked lists and nodes, implementing functions to add nodes , delete from start of the list and show list items

---
+++ chapter7_linked_lists.py
@@ -0,0 +1,86 @@
+class Node():
+
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+    def show_node(self):
+        print(self.value)
+
+
+class LinkedList():
+    
+    def __init__(self):
+        self.first = None
+
+
+    def insert_at_start(self, value):
+        new = Node(value)
+        new.next = self.first
+        self.first = new 
+
+
+    def show(self):
+        if self.list_is_empty():
+            return
+        current = self.first
+        while current != None:
+            current.show_node()
+            current = current.next
+
+
+    def delete_from_start(self):
+        if self.list_is_empty():
+            return
+        temp = self.first
+        
+        self.first = self.first.next
+        return temp
+    
+
+    def list_is_empty(self):
+        if self.first == None:
+            print('List is empty')
+            return True
+        return False
+
+
+test_list = LinkedList()
+test_list.show()
+test_list.insert_at_start(1)
+print(test_list.first)
+test_list.show()
+print()
+test_list.insert_at_start(2)
+print(test_list.first)
+test_list.show()
+print()
+test_list.insert_at_start(3)
+print(test_list.first)
+test_list.insert_at_start(4)
+print(test_list.first)
+test_list.insert_at_start(4)
+print(test_list.first)
+test_list.show()
+print(test_list.first.next.next.next.next.next)
+print(test_list.first.next.next.next.next)
+print(test_list.first.next.next.next)
+print(test_list.first.next.next)
+print(test_list.first.next)
+print(test_list.first)
+test_list.delete_from_start()
+test_list.show()
+print()
+test_list.delete_from_start()
+test_list.show()
+print()
+test_list.delete_from_start()
+test_list.show()
+print()
+test_list.delete_from_start()
+test_list.show()
+print()
+test_list.delete_from_start()
+test_list.show()
+
