d5a3a2496964a633f348be0bc39d10e04d809e41
Gabriel Valenga <gabrielvalengalemos@yahoo.com>
2026-04-01 08:28:03 -0300

    feat: Creating chapter 7 exercise 1 file: linked list stack

---
+++ chapter7_exercise_one_linked_list_stack.py
@@ -0,0 +1,20 @@
+class LinkedListStack:
+
+    def __init__(self):
+        pass
+
+
+    def __stack_is_empty(self):
+        pass
+
+
+    def push_value(self):
+        pass 
+
+
+    def pop_value_from_the_top(self):
+        pass
+
+
+    def top(self):
+        pass
