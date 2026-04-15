91a3cb0a6409854b3c2ed3788966a26119225c7d
Gabriel Valenga <gabrielvalengalemos@yahoo.com>
2026-04-15 07:58:15 -0300

    feat: Created file binary_tree

---
+++ chapter10_binary_tree.py
@@ -0,0 +1,19 @@
+class Node:
+    def __init__(self, value):
+        value = value
+        self.left = None
+        self.right = None 
+
+
+    def show_node(self):
+        print(self.value)
+
+
+class BinaryTree:
+    def __init__(self):
+        self.root = None
+
+    # O(log n) = average | O(n) = worst 
+    def insert():
+        pass # to be implemented
+    
