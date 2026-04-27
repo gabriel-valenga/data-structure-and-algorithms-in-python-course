cb1c814a8c7971cb0e5d917c77b23e90ddd7d241
Gabriel Valenga <gabrielvalengalemos@yahoo.com>
2026-04-27 08:14:14 -0300

    feat: Created file for new chapter: Graphs, and started implementation of  Romanian cities example

---
+++ chapter11_graphs.py
@@ -0,0 +1,88 @@
+class Vertex:
+    def __init__(self, label):
+        self.label = label
+        self.visited = False
+        self.adjacents = []
+
+    
+    def add_adjacent(self, adjacent):
+        self.adjacents.append(adjacent)
+
+
+    def show_adjacents(self):
+        for adjacent in self.adjacents:
+            print(f'adjacent label: {adjacent.vertex.label} | adjacent cost: {adjacent.cost}')
+
+
+class Adjacent:
+    def __init__(self, vertex, cost):
+        self.vertex = vertex
+        self.cost = cost
+
+
+class CitiesOfRomaniaGraphExample:
+    arad = Vertex(label='Arad')
+    zerind = Vertex(label='Zerind')
+    oradea = Vertex(label='Oradea')
+    sibiu = Vertex(label='Sibiu')
+    timisoara = Vertex(label='Timisoara')
+    lugoj = Vertex(label='Lugoj')
+    mehadia = Vertex(label='Mehadia')
+    dobreta = Vertex(label='Dobreta')
+    craiova = Vertex(label='Craiova')
+    rimnicu = Vertex(label='Rimnicu')
+    fagaras = Vertex(label='Fagaras')
+    pitesti = Vertex(label='Pitesti')
+    bucharest = Vertex(label='Bucharest')
+    giurgiu = Vertex(label='Giurgiu')
+
+    arad.add_adjacent(adjacent=Adjacent(vertex=zerind, cost=75))
+    arad.add_adjacent(adjacent=Adjacent(vertex=sibiu, cost=140))
+    arad.add_adjacent(adjacent=Adjacent(vertex=timisoara, cost=118))
+
+    zerind.add_adjacent(adjacent=Adjacent(vertex=arad, cost=75))
+    zerind.add_adjacent(adjacent=Adjacent(vertex=oradea, cost=71))
+
+    oradea.add_adjacent(adjacent=Adjacent(vertex=zerind, cost=71))
+    oradea.add_adjacent(adjacent=Adjacent(vertex=sibiu, cost=151))
+
+    sibiu.add_adjacent(adjacent=Adjacent(vertex=oradea, cost=151))
+    sibiu.add_adjacent(adjacent=Adjacent(vertex=arad, cost=140))
+    sibiu.add_adjacent(adjacent=Adjacent(vertex=fagaras, cost=99))
+    sibiu.add_adjacent(adjacent=Adjacent(vertex=rimnicu, cost=80))
+
+    timisoara.add_adjacent(adjacent=Adjacent(vertex=arad, cost=118))
+    timisoara.add_adjacent(adjacent=Adjacent(vertex=lugoj, cost=111))
+
+    lugoj.add_adjacent(adjacent=Adjacent(vertex=timisoara, cost=111))
+    lugoj.add_adjacent(adjacent=Adjacent(vertex=mehadia, cost=70))
+
+    mehadia.add_adjacent(adjacent=Adjacent(vertex=lugoj, cost=70))
+    mehadia.add_adjacent(adjacent=Adjacent(vertex=dobreta, cost=75))
+
+    dobreta.add_adjacent(adjacent=Adjacent(vertex=mehadia, cost=75))
+    dobreta.add_adjacent(adjacent=Adjacent(vertex=craiova, cost=120))
+
+    craiova.add_adjacent(adjacent=Adjacent(vertex=dobreta, cost=120))
+    craiova.add_adjacent(adjacent=Adjacent(vertex=pitesti, cost=138))
+    craiova.add_adjacent(adjacent=Adjacent(vertex=rimnicu, cost=146))
+
+    rimnicu.add_adjacent(adjacent=Adjacent(vertex=craiova, cost=146))
+    rimnicu.add_adjacent(adjacent=Adjacent(vertex=sibiu, cost=80))
+    rimnicu.add_adjacent(adjacent=Adjacent(vertex=pitesti, cost=97))
+
+    fagaras.add_adjacent(adjacent=Adjacent(vertex=sibiu, cost=99))
+    fagaras.add_adjacent(adjacent=Adjacent(vertex=bucharest, cost=211))
+
+    pitesti.add_adjacent(adjacent=Adjacent(vertex=rimnicu, cost=97))
+    pitesti.add_adjacent(adjacent=Adjacent(vertex=craiova, cost=138))
+    pitesti.add_adjacent(adjacent=Adjacent(vertex=bucharest, cost=101))
+
+    bucharest.add_adjacent(adjacent=Adjacent(vertex=fagaras, cost=211))
+    bucharest.add_adjacent(adjacent=Adjacent(vertex=pitesti, cost=101))
+    bucharest.add_adjacent(adjacent=Adjacent(vertex=giurgiu, cost=90))
+
+
+romania_cities = CitiesOfRomaniaGraphExample()
+romania_cities.arad.show_adjacents()
+romania_cities.bucharest.show_adjacents()
