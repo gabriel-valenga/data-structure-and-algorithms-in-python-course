dc7a5ccd9ec741310ba64d3eac920032fa16433c
Gabriel Valenga <gabrielvalengalemos@yahoo.com>
2026-05-04 08:52:56 -0300

    feat: Creating Djikstra algorythm class, with adjacency matrix

--- chapter11_graphs.py
+++ chapter11_graphs.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from chapter6_circular_queue import CircularQueue
 from chapter6_stack import Stack
 from chapter5_sorted_arrrays import SortedArray
@@ -320,6 +322,53 @@ class AStarSearch:
                 self.search(sorted_array.values[0].vertex)
 
         
+class Djikstra():
+    vertices = {
+        'arad': 0, 'zerind': 1, 'oradea': 2, 'sibiu': 3, 'timisoara': 4,
+        'lugoj': 5, 'mehadia': 6, 'dobreta': 7, 'craiova': 8, 'rimnicu': 9,
+        'fagaras': 10, 'pitesti': 11, 'bucharest': 12, 'giurgiu': 13
+    }
+    cities = {
+        0: 'arad', 1: 'zerind', 2: 'oradea', 3: 'sibiu', 4: 'timisoara',
+        5: 'lugoj', 6: 'mehadia', 7: 'dobreta', 8: 'craiova', 9: 'rimnicu',
+        10: 'fagaras', 11: 'pitesti', 12: 'bucharest', 13: 'giurgiu'
+    }
+    edges = np.zeros([len(cities), len(cities)], dtype=int)
+    edges[vertices['arad'], vertices['zerind']] = 75
+    edges[vertices['arad'], vertices['sibiu']] = 140
+    edges[vertices['arad'], vertices['timisoara']] = 118
+    edges[vertices['zerind'],[vertices['arad']]] = 75
+    edges[vertices['zerind'],[vertices['oradea']]] = 71
+    edges[vertices['oradea'],[vertices['zerind']]] = 71
+    edges[vertices['oradea'],[vertices['sibiu']]] = 151
+    edges[vertices['sibiu'],[vertices['oradea']]] = 151
+    edges[vertices['sibiu'],[vertices['arad']]] = 140
+    edges[vertices['sibiu'],[vertices['fagaras']]] = 99
+    edges[vertices['sibiu'],[vertices['rimnicu']]] = 80
+    edges[vertices['timisoara'],[vertices['arad']]] = 118
+    edges[vertices['timisoara'],[vertices['lugoj']]] = 111
+    edges[vertices['lugoj'],[vertices['timisoara']]] = 111
+    edges[vertices['lugoj'],[vertices['mehadia']]] = 70
+    edges[vertices['mehadia'],[vertices['lugoj']]] = 70
+    edges[vertices['mehadia'],[vertices['dobreta']]] = 75
+    edges[vertices['dobreta'],[vertices['mehadia']]] = 75
+    edges[vertices['dobreta'],[vertices['craiova']]] = 120
+    edges[vertices['craiova'],[vertices['dobreta']]] = 120
+    edges[vertices['craiova'],[vertices['pitesti']]] = 138
+    edges[vertices['craiova'],[vertices['rimnicu']]] = 146
+    edges[vertices['rimnicu'],[vertices['craiova']]] = 146
+    edges[vertices['rimnicu'],[vertices['sibiu']]] = 80
+    edges[vertices['rimnicu'],[vertices['pitesti']]] = 97
+    edges[vertices['fagaras'],[vertices['sibiu']]] = 99
+    edges[vertices['fagaras'],[vertices['bucharest']]] = 211
+    edges[vertices['pitesti'],[vertices['rimnicu']]] = 97
+    edges[vertices['pitesti'],[vertices['craiova']]] = 138
+    edges[vertices['pitesti'],[vertices['bucharest']]] = 101
+    edges[vertices['bucharest'],[vertices['fagaras']]] = 211
+    edges[vertices['bucharest'],[vertices['pitesti']]] = 101
+    edges[vertices['bucharest'],[vertices['giurgiu']]] = 90
+
+
 romania_cities = CitiesOfRomaniaGraphExample()
 romania_cities_straight_line_distance = CitiesOfRomaniaStraightLineDistanceGraphExample()
 
@@ -377,4 +426,3 @@ def a_star_search_example():
     a_star_search = AStarSearch(destination=romania_cities_straight_line_distance.bucharest)
     a_star_search.search(romania_cities_straight_line_distance.arad)
 
-a_star_search_example()
